082d2207e0cc701f7e6f1560b4e06350395c7d7d
Lyuben Petrov <lyuben.y.petrov@gmail.com>
2015-10-11 21:16:29 +0300

    Add Videos

--- web/router.ex
+++ web/router.ex
@@ -20,8 +20,14 @@ defmodule Rumbl.Router do
     get "/", PageController, :index
     resources "/users", UserController
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+    resources "/videos", VideoController
   end
 
+  scope "/manage", Rumbl do
+    pipe_through [:browser, :authenticate_user]
+
+    resources "/videos", VideoController
+  end
   # Other scopes may use custom stacks.
   # scope "/api", Rumbl do
   #   pipe_through :api
